33baa6b9927646178a3c0df860a403eb2fabdd3e
MR <MR@DESKTOP-71NRBDS>
2021-11-24 11:42:56 +0700

    First

---
+++ src/Day_3.java
@@ -0,0 +1,13 @@
+
+public class Day_3 {
+
+	public static void main(String[] args) {
+		 int n = 9;
+		 for (int i=0;i<=10;i++) {
+
+		 System.out.printf("%d x %d = %d%n",n,i, n*i);
+		 }
+
+	}
+
+}
